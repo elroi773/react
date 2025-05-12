8ed022ce41ed71fd61c93d9f004d2d6cde5c2a7b
elroi773 <s2457@e-mirim.hs.kr>
2025-05-12 23:42:06 +0900

    dropdown

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// src/Dropdown.jsx
+import React from 'react';
+
+function Dropdown() {
+  return (
+    <select>
+      <option value="apple">Apple</option>
+      <option value="banana">Banana</option>
+      <option value="cherry">Cherry</option>
+    </select>
+  );
+}
+
+export default Dropdown;
