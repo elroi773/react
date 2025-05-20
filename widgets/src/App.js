fd8cedb4c9d882bb6f07aef95be57b3f67641543
elroi773 <s2457@e-mirim.hs.kr>
2025-05-20 11:36:30 +0900

    widgets

---
+++ widgets/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Widget } from "./Widget";
+import "./style.css"; // 스타일 import
+
+function App() {
+  return (
+    <div className="App">
+      <Widget />
+    </div>
+  );
+}
+
+export default App;
