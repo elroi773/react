2831b3cd74c2660e3d54b2e163ba92c0687c469c
elroi773 <s2457@e-mirim.hs.kr>
2025-05-26 13:20:18 +0900

    trashbutton

---
+++ trashbtn/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TrashButton from "./TrashButton";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trash Button Test</h1>
+      <TrashButton />
+    </div>
+  );
+}
+
+export default App;
