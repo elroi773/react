70654fe2e1dacf6122d36c5810465fe65d5eacfb
elroi773 <s2457@e-mirim.hs.kr>
2025-05-29 14:39:36 +0900

    it show 오프닝 데모 버전

---
+++ crystal-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CrystalBallScene from "./CrystalBallScene.jsx";
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", background: "black" }}>
+      <CrystalBallScene />
+    </div>
+  );
+}
+
+export default App;
