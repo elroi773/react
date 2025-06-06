e87f7dcc3f47901cb79c9ed3b1fba9af166f19c2
elroi773 <s2457@e-mirim.hs.kr>
2025-06-06 21:45:41 +0900

    프로필 수정

---
+++ editable-text/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import { EditableTitleExample } from "./components/EditableTextExample";
+
+function App() {
+  return (
+    <div className="App">
+      <EditableTitleExample />
+    </div>
+  );
+}
+
+export default App;
