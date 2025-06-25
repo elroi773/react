0e49bca7dfe49f6efce22c58c323610cccce095b
elroi773 <s2457@e-mirim.hs.kr>
2025-06-25 11:13:28 +0900

    itshow

---
+++ it-show-demo/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import CrystalBall from './CrystalBall'
+
+// App.jsx
+export default function App() {
+    return (
+      <Canvas>
+        <CrystalBall />
+      </Canvas>
+    )
+  }
+  
