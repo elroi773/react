251ee260c8028922cffefe81174081e7f804a751
elroi773 <s2457@e-mirim.hs.kr>
2025-07-01 09:05:55 +0900

    parallax

---
+++ parallax/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Parallax } from "./Parallax"; // Parallax 컴포넌트 불러오기
+
+function App() {
+  return (
+    <div className="App">
+      <Parallax /> {/* Parallax 컴포넌트 렌더링 */}
+    </div>
+  );
+}
+
+export default App;
