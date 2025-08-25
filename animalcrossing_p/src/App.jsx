a911a484ad6b6ff41b66f7b3f9c5a37906d50495
elroi773 <s2457@e-mirim.hs.kr>
2025-08-25 13:32:49 +0900

    api key 추가 해서 주민 불러오기 성공 ㅎㅎ

--- animalcrossing_p/src/App.jsx
+++ animalcrossing_p/src/App.jsx
@@ -1,44 +1,46 @@
 import axios from 'axios';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { div } from 'three/tsl';
-import VillagerList from './VillagerList';
+import VillagerList from './VillagerList.jsx';
 
-function App(){
-  const [data,setData] = useState([]);
-  const[loading, setLoading] = useState(true);
-  const[error,setError] = useState(null);
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const fetchData = async () =>{
-    const URL = "https://api/nookipedia.com/villagers";
+  const fetchData = async () => {
+    const URL = "https://api.nookipedia.com/villagers";
 
-    try{
+    try {
       setLoading(true);
-      const response = await axios.get(URL,{
-        headers:{
-          "X-API-KEY": process.env.REACT_APP_API_KEY,
-          "Accept-Version" : "1.0.0",
+      const response = await axios.get(URL, {
+        headers: {
+          "X-API-KEY": "api key",
+          "Accept-Version": "1.0.0",
         },
       });
       setData(response.data);
-      console.log(response);
-    }catch(error){
+      console.log(response.data);
+    } catch (error) {
       setError(error.message);
-    }finally{
+    } finally {
       setLoading(false);
     }
-  }
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
-  if(loading) return <p>Loading...</p>
-  if(error) return <p>Error : {error}</p>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error}</p>;
 
-  return(
+  return (
     <div className="container">
       <h1>Animal Crossing</h1>
-      <VillagerList />
+      <VillagerList villagers={data} />
     </div>
-  )
+  );
 }
+
+export default App;
