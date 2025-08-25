a69ce924bff8d477c9f62ab19e767ade7b381dc4
elroi773 <s2457@e-mirim.hs.kr>
2025-08-25 13:34:10 +0900

    api key 추가 해서 주민 불러오기 성공 ㅎㅎ

---
+++ animalcrossing_p/src/VillagerList.jsx
@@ -0,0 +1,13 @@
+export default function VillagerList({ villagers }) {
+    return (
+      <ul className="villager-list">
+        {villagers.map((villager, idx) => (
+          <li key={idx} className="villager-item">
+            <img src={villager.image_url} alt={villager.name} />
+            <h2>{villager.name}</h2>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  
